87e34a9a54d0be3a3e49cb6de33889c7745c5871
AmineDev1 <45659795+AmineDev1@users.noreply.github.com>
2018-12-21 12:52:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,57 +1,53 @@
 const Discord = require('discord.js');
+const client = new Discord.Client();
 
-const Util = require('discord.js');
 
-const getYoutubeID = require('get-youtube-id');
-
-const fetchVideoInfo = require('youtube-info');
-
-const YouTube = require('simple-youtube-api');
-
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-
-const queue = new Map();
-
-const ytdl = require('ytdl-core');
-
-const fs = require('fs');
-
-const gif = require("gif-search");
-
-const client = new Discord.Client({disableEveryone: true});
-
-var adminprefix = 's1'
-/////////////////////////
-////////////////////////
-
-const developers = ["417377495160193044","462434603895095297"]
+const adminprefix = "+";
+const devs = ['462434603895095297','417377495160193044'];
 client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'aaaa')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
-     if (message.content === (adminprefix + "aaaa")) {
-    message.guild.leave();        
-  } else  
-  if (message.content.startsWith(adminprefix + 'aaaa')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'aaaa')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'setT')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/dream");
-      message.channel.send(`**✅**`)
-  }
-  if (message.content.startsWith(adminprefix + 'aaaa')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**${argresult}** `)
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + '.')) {
+  client.user.setGame(argresult);
+} else 
+  if (message.content.startsWith(adminprefix + '.')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
 } else
+  if (message.content.startsWith(adminprefix + '.')) {
+client.user.setAvatar(argresult);
+      } else     
+  if (message.content.startsWith(adminprefix + '.')) {
+        if (!devs.includes(message.author.id)) return;
+  client.user.setActivity(argresult, {type:'WATCHING'});
+  } else
+  if (message.content.startsWith(adminprefix + '.')) {
+        if (!devs.includes(message.author.id)) return;
+  client.user.setActivity(argresult , {type:'LISTENING'});
+  } else    
+if (message.content.startsWith(adminprefix + 'setT')) {
+      if (!devs.includes(message.author.id)) return;
+  client.user.setGame(argresult, "https://www.twitch.tv/uzii");//حقوق دايموند كودزحقوق دايموند كودز
+}
+});
+
+
+var prefix = "@";
+
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
 
 
 client.login(process.env.BOT_TOKEN);
